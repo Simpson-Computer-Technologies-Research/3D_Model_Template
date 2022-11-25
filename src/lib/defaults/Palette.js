93fda127bdeb39f2206dc54f385c7c47951f5185
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-25 18:20:58 -0500

    Add files via upload

---
+++ src/lib/defaults/Palette.js
@@ -0,0 +1,44 @@
+import { THREE } from "$lib/Imports.js";
+
+// Light Mode Phone Wallpapers
+export const HOME_WALLPAPER = new THREE.TextureLoader().load("./textures/wallpaper.png");
+
+// Dark Mode Phone Wallpapers
+export const DARK_HOME_WALLPAPER = new THREE.TextureLoader().load("./textures/wallpaper.png");
+
+// To check whether the wallpaper is already changed
+export let IsWallpaperChanged = false;
+
+// Set the current wallpaper
+export let Wallpapers = {
+    Home: HOME_WALLPAPER
+};
+
+// Convert the color to light mode
+export const ToLightMode = () => {
+    Wallpapers = {
+        Home: HOME_WALLPAPER,
+    }
+}
+
+// Convert the color to dark mode
+export const ToDarkMode = () => {
+    Wallpapers = {
+        Posts: DARK_HOME_WALLPAPER,
+    }
+}
+
+// Phone Wallpaper Manipulation
+export const UpdatePhoneWallpaper = (model, wallpaper) => {
+	model.traverse((obj) => {
+		if (obj.name == "Body_Wallpaper_0") {
+			obj.material.map = wallpaper;
+			obj.material.needsUpdate = true;
+		};
+	});
+    // Update the wallpaper change status
+    IsWallpaperChanged = !IsWallpaperChanged;
+
+    // Return the updated model
+	return model;
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	// Imports
+	import { LoadPhoneObjectWithGSAP } from "$lib/PhoneObject.js";
+	import { SetScene } from "$lib/defaults/Scene.js";
+	import { Palette } from "$lib/Imports.js";
+	import { onMount } from "svelte";
+    
+	// When the user loads up the website...
+	let ThreeObject;
+    onMount(async () => {
+		let Scene = await SetScene(ThreeObject);
+		await LoadPhoneObjectWithGSAP(Scene, Palette.Wallpapers.Posts);
+    });
+</script>
+
+<!--THREEJS Object Loading Header -->
+<div id="loading-header" class="fade-in">
+	<h2 class="flex justify-center items-center mt-96 text-gray-700 text-3xl font-black tracking-widest">LOADING<mark style="color: #35d0ff; background: none;">&nbsp;OBJECTS</mark></h2>
+	<div class="mx-[40rem] h-1 flex justify-center items-center bg-gray-200 mt-10 rounded-full"></div>
+</div>
+
+<!-- Main Header -->
+<div id="main-div" class="fade-in hidden">
+	<h2 class="flex justify-center items-center my-60">Loaded</h2>
+</div>
+
+<!-- The 3D Phone -->
+<canvas bind:this={ThreeObject} class="fade-in fixed top-0 xl:ml-96 outline-none h-screen w-screen"/>
+
+<!-- Styling -->
+<style>
+	.fade-out { animation: fade_out 1s; opacity: 0; }
+	.fade-in { animation: fade_in 0.5s; opacity: 1; }
+	@keyframes fade_out { from { opacity: 1; } to { opacity: 0; } }
+	@keyframes fade_in { from { opacity: 0; } to { opacity: 1; } }
+</style>
